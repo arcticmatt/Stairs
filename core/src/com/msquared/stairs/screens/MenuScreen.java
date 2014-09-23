2f28d700a3de8cc0621d2d0f1288c2233ee86676
arcticmatt <mlimcollege@gmail.com>
2014-09-22 21:00:46 -0700

    use 72 frame animations

--- core/src/com/msquared/stairs/screens/MenuScreen.java
+++ core/src/com/msquared/stairs/screens/MenuScreen.java
@@ -25,7 +25,7 @@ import com.msquared.stairs.view.WorldRenderer;
 
 public class MenuScreen extends AbstractScreen implements Screen {
 	CharSequence gameScore;
-	private static final float FRAME_DURATION = 0.083f;
+	private static final float FRAME_DURATION = 0.055f;
 	private Animation stairsAnimation;
 	private Animation stairsAnimationInverted;
 	float stateTime;
@@ -115,7 +115,7 @@ public class MenuScreen extends AbstractScreen implements Screen {
 	}
 
 	private void loadTextures() {
-		int num = 48;
+		int num = 72;
 		// Can't dispose of these, otherwise the animations disappear
 		TextureAtlas atlas;
 		TextureAtlas atlasInverted;
